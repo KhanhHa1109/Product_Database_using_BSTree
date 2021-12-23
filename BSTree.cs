a9815964e44dc7c2ffcce891fda384ab84b19443
KhanhHa1109 <nguyenquynhkhanhha@gmail.com>
2021-12-24 01:15:38 +0700

    update

---
+++ BSTree.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCTDL
+{
+    public class BSTree
+    {
+        private int size;
+        public Node root;
+
+        public BSTree()
+        {
+            size = 0;
+            root = null;
+        }
+
+        public int Size()
+        {
+            return size;
+        }
+
+        #region Insert
+        public void Insert(Node newNode)
+        {
+            root = Insert(root, newNode);
+        }
+
+        private Node Insert(Node node, Node newNode)
+        {
+            if (node == null)
+            {
+                size++;
+                return newNode;
+            }
+
+            if (newNode.lessThanOrEqual(node.product))
+            {
+                node.left = Insert(node.left, newNode);
+            }
+            else
+            {
+                node.right = Insert(node.right, newNode);
+            }
+            return node;
+        }
+        #endregion
+
+        #region Find
+        public Node Find(int productId)
+        {
+            return Find(this.root, productId);
+        }
+
+        private Node Find(Node node, int productId)
+        {
+            if (node == null) return null;
+
+
+            if (node.hasproduct(productId))
+            {
+                return node;
+            }
+            if (node.lessThan(productId))
+            {
+                return Find(node.right, productId);
+            }
+            else
+            {
+                return Find(node.left, productId);
+            }
+        }
+        #endregion
+
+        #region Delete
+        public void Delete(int productId)
+        {
+            root = Delete(root, productId);
+        }
+
+        public Node Delete(Node node, int productId)
+        {
+            if (node == null) return null;
+
+
+            if (node.hasproduct(productId))
+            {
+                size--;
+                if (node.left == null)
+                {
+                    return node.right;
+                }
+                Node maxNode = node.left;
+                Node preNode = maxNode;
+                while (maxNode.right != null)
+                {
+                    preNode = maxNode;
+                    maxNode = maxNode.right;
+                }
+
+                maxNode.right = node.right;
+                if (maxNode != node.left)
+                {
+                    preNode.right = maxNode.left;
+                    maxNode.left = node.left;
+
+                }
+                return maxNode;
+            }
+            if (node.lessThan(productId))
+            {
+                node.right = Delete(node.right, productId);
+            }
+            else
+            {
+                node.left = Delete(node.left, productId);
+            }
+            return node;
+
+        }
+        #endregion
+
+        #region TraversePreOrder
+        public void TraversePreOrder()
+        {
+            Console.Write("\n\n----------------------------------------TraversePreOrder---------------------------------------");
+            TraversePreOrder(root);
+        }
+
+        private void TraversePreOrder(Node node)
+        {
+            if (node == null) return;
+            node.Data();
+            TraversePreOrder(node.left);
+            TraversePreOrder(node.right);
+        }
+        #endregion
+
+        #region TraverseInOrder
+        public void TraversetInOrder()
+        {
+            Console.Write("\n\n----------------------------------------TraverseInOrder----------------------------------------");
+            TraverseInOrder(root);
+        }
+
+        private void TraverseInOrder(Node node)
+        {
+            if (node == null) return;
+            TraverseInOrder(node.left);
+            node.Data();
+            TraverseInOrder(node.right);
+        }
+        #endregion
+
+        #region TraversePostOrder
+        public void TraversePostOrder()
+        {
+            Console.Write("\n\n----------------------------------------TraversePostOrder----------------------------------------");
+            TraversePostOrder(root);
+        }
+
+        private void TraversePostOrder(Node node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            TraversePostOrder(node.left);
+            TraversePostOrder(node.right);
+            node.Data();
+        }
+        #endregion
+
+        #region Max Depth
+        public int maxDepth(Node node)
+        {
+            if (node == null)
+                return -1;
+            else
+            {
+                int leftDep = maxDepth(node.left);
+                int rightDep = maxDepth(node.right);
+
+                if (leftDep > rightDep)
+                {
+                    return (leftDep + 1);
+                }
+                else
+                {
+                    return (rightDep + 1);
+                }
+            }
+        }
+        public int maxDepth()
+        {
+            return maxDepth(this.root);
+        }
+        #endregion
+
+        #region Min Depth
+        public int minDepth(Node node)
+        {
+            if (node == null)
+                return -1;
+            else
+            {
+                int leftDep = minDepth(node.left);
+                int rightDep = minDepth(node.right);
+
+                if (leftDep < rightDep)
+                {
+                    return (leftDep + 1);
+                }
+                else
+                {
+                    return (rightDep + 1);
+                }
+            }
+        }
+        public int minDepth()
+        {
+            return minDepth(this.root);
+        }
+        #endregion
+
+        #region Count node
+        public int numNodesIn(Node node)
+        {
+            if (node == null) return 0;
+            return 1 + numNodesIn(node.left) + numNodesIn(node.right);
+        }
+        public int numNodesIn()
+        {
+            return numNodesIn(this.root);
+        }
+        #endregion
+
+        #region Count edge
+        public int numEdgesIn(Node node)
+        {
+            return node == null ? 0 : numNodesIn(node) - 1;
+        }
+        public int numEdgesIn()
+        {
+            return numEdgesIn(this.root);
+        }
+        #endregion
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnCTDL
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Clear();
+
+            // Insert Product
+            Console.Write("**Product Database using Binary Search Tree** \n");
+            BSTree tree = new BSTree();
+            tree.Insert(new Node(new Product(18, "Dell XPS 15 9510", 180, 1, "Up-to-date")));
+            tree.Insert(new Node(new Product(20, "Ram DDR4", 980, 5, "Fast")));
+            tree.Insert(new Node(new Product(10, "SSD 256gb", 1000, 4, "Reasonable")));
+            tree.Insert(new Node(new Product(15, "HDD 1T", 560, 3, "Cheap")));
+            tree.Insert(new Node(new Product(12, "VGA NVIDIA", 3800, 4, "Cool")));
+            tree.Insert(new Node(new Product(17, "USB 16gb", 250, 3, "Great")));
+            tree.Insert(new Node(new Product(7, "Dell Vostro 5490", 18000, 4, "Versatile")));
+            tree.Insert(new Node(new Product(8, "Surface Pro 3", 15000, 3, "Expensive")));
+            tree.Insert(new Node(new Product(3, "Logitech Mouse", 480, 3, "Good")));
+
+
+            // Create MENU   
+            for (; ; )
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("| 1 - Insert   |\n| 2 - Traverse |\n| 3 - Find     |\n| 4 - Remove   |\n| 5 - DepthTree|\n| 0 - Exit     |");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write("\n\n---Choose number: ");
+                string sel = Console.ReadLine();
+                int _Id;
+                int _Price;
+                string _Productname;
+                int _Rating;
+                string _Des;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                switch (sel)
+                {
+                    case "1":
+                        Console.Write("\nInsert product information: ");
+                        Console.Write("\nId = ");
+                        _Id = int.Parse(Console.ReadLine());
+                        Console.Write("Product Name = ");
+                        _Productname = Console.ReadLine();
+                        Console.Write("Product Price = ");
+                        _Price = int.Parse(Console.ReadLine());
+                        Console.Write("Product Rating = ");
+                        _Rating = int.Parse(Console.ReadLine());
+                        Console.Write("Product Description = ");
+                        _Des = Console.ReadLine();
+                        tree.Insert(new Node(new Product(_Id, _Productname, _Price, _Rating, _Des)));
+                        Console.ReadLine();
+                        Console.Clear();
+                        continue;
+                    case "2":
+                        try
+                        {
+                            Console.WriteLine("\n>> Count nodes: " + tree.numNodesIn());
+                            Console.WriteLine(">> Count edges: " + tree.numEdgesIn());
+                            Console.WriteLine("\nChoose number:");
+                            Console.Write(">> [1] -- Inorder\n>> [2] -- Preorder\n>> [3] -- Postorder\n");
+                            Console.Write("Choose number: ");
+                            sel = Console.ReadLine();
+                            switch (sel)
+                            {
+                                case "1":
+                                    tree.TraversetInOrder();
+                                    break;
+                                case "2":
+                                    tree.TraversePreOrder();
+                                    break;
+                                case "3":
+                                    tree.TraversePostOrder();
+                                    break;
+                                default:
+                                    Console.WriteLine(">> Try again please!");
+                                    continue;
+                            }
+                            Console.ReadLine();
+                            Console.Clear();
+                            continue;
+                        }
+                        catch
+                        {
+                            Console.WriteLine(">> BSTree not found!");
+                            Console.ReadLine();
+                            Console.Clear();
+                            continue;
+                        }
+                    case "3":
+                        try
+                        {
+                            Console.Write("Enter product Id : ");
+                            _Id = int.Parse(Console.ReadLine());
+                            Node node = tree.Find(_Id);
+                            if (node == null)
+                            {
+                                Console.WriteLine("\n\n>> No results. Please try again!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\n\n---Find result = " + _Id);
+                                node.Data();
+                            }
+
+                            Console.ReadLine();
+                            Console.Clear();
+                            continue;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\n\n>> No results. Please try again!");
+
+                            Console.ReadLine();
+                            Console.Clear();
+                            continue;
+                        }
+                    case "4":
+                        try
+                        {
+                            Console.Write("Enter product Id: ");
+                            _Id = int.Parse(Console.ReadLine());
+                            Node node = tree.Find(_Id);
+                            if (node == null)
+                            {
+                                Console.WriteLine("\n\n>> No results. Please try again!");
+                            }
+                            else
+                            {
+                                tree.Delete(_Id);
+                                Console.WriteLine("\n\n---Delete product have Id = " + _Id);
+                                //Show Preorder
+                                tree.TraversePreOrder();
+
+                                //Show Postorder
+                                //tree.TraversePostOrder();
+                                //Show Inorder
+                                //tree.TraversetInOrder();
+                            }
+
+                            Console.ReadLine();
+                            Console.Clear();
+                            continue;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("\n\n>> No results. Please try again!");
+
+                            Console.ReadLine();
+                            Console.Clear();
+                            continue;
+                        }
+                    case "5":
+                        {
+                            Console.Write("\n>> Max Depth: " + tree.maxDepth());
+                            Console.Write("\n>> Min Depth: " + tree.minDepth());
+
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+                        }
+                    case "0":
+                        Environment.Exit(0);
+                        continue;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\n>> Please try another number!");
+                        Console.ReadLine();
+                        Console.Clear();
+                        break;
+                }
+            }
+        }
+    }
+}
